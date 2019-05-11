2f8a6ef20e4a1edd7dd6e95d652289ef8aab8fa5
Dillon Kearns <dillon@dillonkearns.com>
2019-05-10 19:39:10 -0700

    Create single case expression.

--- client/src/Timer.elm
+++ client/src/Timer.elm
@@ -25,24 +25,24 @@ selection =
 
 view : Time.Posix -> Maybe Timer -> Element msg
 view now maybeTimer =
-    case maybeTimer of
-        Just timer ->
-            case
-                RemainingSeconds.between
-                    { now = now
-                    , timerCreated = timer.createdAt
-                    , timerDurationMinutes = 25
-                    }
-            of
-                Just remaining ->
-                    Element.column []
-                        [ Element.text (Api.Enum.TimerKind.toString timer.kind)
-                        , Element.text "Timer is running"
-                        , Element.text (remaining |> RemainingSeconds.toString)
-                        ]
-
-                Nothing ->
-                    Element.text "No active timer"
+    case
+        maybeTimer
+            |> Maybe.andThen
+                (\timer ->
+                    RemainingSeconds.between
+                        { now = now
+                        , timerCreated = timer.createdAt
+                        , timerDurationMinutes = 25
+                        }
+                        |> Maybe.map (Tuple.pair timer)
+                )
+    of
+        Just ( timer, remaining ) ->
+            Element.column []
+                [ Element.text (Api.Enum.TimerKind.toString timer.kind)
+                , Element.text "Timer is running"
+                , Element.text (remaining |> RemainingSeconds.toString)
+                ]
 
         Nothing ->
             Element.text "No active timer"
